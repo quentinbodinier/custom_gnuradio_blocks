7b15594bbbb42940fd8f4a22c166dfe64ce8675a
Quentin Bodinier <quentin.bodinier@supelec.fr>
2017-02-15 18:07:10 +0100

    Adding possibility to set gain in dB

--- python/gain_sweeper.py
+++ python/gain_sweeper.py
@@ -26,29 +26,39 @@ class gain_sweeper(gr.sync_block):
     """
     Gain Sweeper block
     """
-    def __init__(self, gain_range,segment_length):
+    def __init__(self, gain_range,segment_length, gain_mode):
         """
         Makes gain sweeper block
 
         Args:
             gain_range: list of gains to iteratively apply to the input signal
             segment_length: gain increases all segment_length samples
+            mode: linear or dB
         """
         gr.sync_block.__init__(self,
             name="gain_sweeper",
             in_sig=[np.complex64],
             out_sig=[np.complex64])
 
-        self.gain_range = gain_range
+       
         self.segment_length = segment_length
+        self.gain_mode = gain_mode
         self.k=0
+        if gain_mode == 'linear':
+            self.gain_range = np.array(gain_range)
+        elif gain_mode == 'dB':
+            self.gain_range = np.power(10,np.array(gain_range)/10.)
 
 
     def work(self, input_items, output_items):
         in0 = input_items[0]
         out = output_items[0]
-        # <+signal processing here+>
+        
+        # We simply multiply the input by a gain that we take from the gain range
         out[:] = in0*self.gain_range[(self.k/self.segment_length)%len(self.gain_range)]
         self.k += 1 
+        if self.k%self.segment_length==0:
+            print 'Current gain: %s' % self.gain_range[(self.k/self.segment_length)%len(self.gain_range)]
+        
         return len(output_items[0])
 
