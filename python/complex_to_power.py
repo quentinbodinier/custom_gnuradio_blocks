ca36534b9b6f6a027bc50028f1155d1e2421e236
Quentin Bodinier <quentin.bodinier@supelec.fr>
2017-03-20 22:00:49 +0100

    turned complex to power into decimation block

--- python/complex_to_power.py
+++ python/complex_to_power.py
@@ -12,7 +12,7 @@ class complex_to_power(gr.sync_block):
         gr.decim_block.__init__(self,
             name="complex_to_power",
             in_sig=[(np.complex,vlen)],
-            out_sig=[(np.float,vlen)], 2)
+            out_sig=[(np.float,vlen)], decim=2)
 
 
     def work(self, input_items, output_items):
