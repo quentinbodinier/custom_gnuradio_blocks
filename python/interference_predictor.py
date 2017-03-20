676605abc33a6a2a729c9764e41e504b00bb81b2
Quentin Bodinier <quentin.bodinier@supelec.fr>
2017-03-20 21:19:02 +0100

    Adding moving average block

--- python/interference_predictor.py
+++ python/interference_predictor.py
@@ -5,7 +5,7 @@ class interference_predictor(gr.sync_block):
     """
     docstring for block interference_predictor
     """
-    def __init__(self, transmit_signal, active_subcarriers, active_sec=):
+    def __init__(self, transmit_signal, active_subcarriers, active_sec=0):
         gr.sync_block.__init__(self,
             name="interference_predictor",
             in_sig=None,
