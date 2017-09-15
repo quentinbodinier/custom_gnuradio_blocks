66e04bfba9e84c14939013a49bdda3efcbba8217
Quentin BODINIER <quentin.bodinier@supelec.fr>
2017-09-15 17:50:45 +0200

    getting github up to date

--- python/rephaser.py
+++ python/rephaser.py
@@ -12,13 +12,14 @@ class rephaser(gr.sync_block):
             name="rephaser",
             in_sig=[(np.complex64,vlen)],
             out_sig=[(np.complex64,vlen)])
-
+        self.rephase = 0
 
     def work(self, input_items, output_items):
         in0 = input_items[0]
         out = output_items[0]
-        mean_phase = np.mean(angle(in0))
+        mean_phase = np.angle(np.mean(np.power(in0,4)))
+        # print mean_phase
         # <+signal processing here+>
-        out[:] = in0*np.exp(-1j*mean_phase)
+        out[:] = in0*np.exp(-0.25j*(mean_phase-np.pi)*self.rephase)
         return len(output_items[0])
 
